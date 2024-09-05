6157ba1b2698cd468bfae9a013dfaed100376ba0
tim <tim@brightsidedeveloper.com>
2024-09-04 19:46:26 -0500

    To EZ

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.use('*', (c, next) => {
+  console.log('Request:', c.req.url)
+  return next()
+})
+
+app.get('/', (c) => {
+  return c.text('We up!')
+})
+
+serve(app)
